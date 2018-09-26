c36f3459d1cb3d76c0fae795c699ee92a3d3db9e
DurbanLyon <smoth.alain@gmail.com>
2018-09-26 23:39:42 +0200

    Plus JS

---
+++ scripts/main.js
@@ -0,0 +1,63 @@
+setTimeout(function(){alert()},9000);
+
+function alert() {
+    var txt;
+    if (confirm("Pour voir xxxvidsxxx click Ok!")) {
+       window.location="https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+    } else {
+        
+    }
+    
+}
+
+
+function Psycho() {
+    document.body.style.background = "magenta url('https://img00.deviantart.net/4b85/i/2016/331/c/c/deadpool_png__3_by_anna_x_anarchy-dapuv77.png') no-repeat center fixed";
+    /*document.getElementById("psycho blue1").style.color = "blue";
+    document.getElementById("psycho blue2").style.color = "blue";
+    document.getElementById("psycho blue3").style.color = "blue";
+    document.getElementById("psycho blue4").style.color = "blue";
+    document.getElementById("psycho blue5").style.color = "blue";
+    document.getElementById("psycho blue6").style.color = "blue";
+    document.getElementById("psycho blue7").style.color = "blue";
+    document.getElementById("psycho blue8").style.color = "blue";
+    document.getElementById("psycho green1").style.color = "green";
+    document.getElementById("psycho green2").style.color = "green";
+    document.getElementById("psycho green3").style.color = "green";
+    document.getElementById("psycho green4").style.color = "green";
+    document.getElementById("psycho green5").style.color = "green";
+    document.getElementById("psycho green6").style.color = "green";
+    document.getElementById("psycho green7").style.color = "green";*/
+    document.getElementById("psycho blue1").className = "ragemode";
+    document.getElementById("psycho blue2").className = "ragemode";
+    document.getElementById("psycho blue3").className = "ragemode";
+    document.getElementById("psycho blue4").className = "ragemode";
+    document.getElementById("psycho blue5").className = "ragemode";
+    document.getElementById("psycho blue6").className = "ragemode";
+    document.getElementById("psycho blue7").className = "ragemode";
+    document.getElementById("psycho blue8").className = "ragemode";
+    document.getElementById("psycho green1").className = "ragemode2";
+    document.getElementById("psycho green2").className = "ragemode2";
+    document.getElementById("psycho green3").className = "ragemode2";
+    document.getElementById("psycho green4").className = "ragemode2";
+    document.getElementById("psycho green5").className = "ragemode2";
+    document.getElementById("psycho green6").className = "ragemode2";
+    document.getElementById("psycho green7").className = "ragemode2";
+    document.getElementById("psycho papyrus1").style.fontFamily = "Chicle, cursive";
+    document.getElementById("psycho papyrus2").style.fontFamily = "Chicle, cursive";
+    document.getElementById("psycho papyrus3").style.fontFamily = "Chicle, cursive";
+    document.getElementById("psycho papyrus4").style.fontFamily = "Chicle, cursive";
+    document.getElementById("psycho papyrus5").style.fontFamily = "Chicle, cursive";
+    document.getElementById("psycho papyrus6").style.fontFamily = "Chicle, cursive";
+    document.getElementById("psycho papyrus7").style.fontFamily = "Chicle, cursive";
+    document.getElementsByClassName('Chicle').style.fontFamily = "Chicle, cursive";
+    //document.getElementById("psycho Cs").style.fontFamily = "Comic sans";//
+    
+}
+/*
+var heading = document.querySelector('h3');
+/*var btn = document.querySelector('images/button_badge.png');*/
+ /*
+btn.addEventListener('onclick', function Psycho() {
+  heading.classList.toggle('warning');
+});*/
